f2a392f730fbc752b6e579543b1074fb3020f001
josemlrod <mrjosemlrod@gmail.com>
2023-09-24 13:39:06 -0400

    create Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Layout } from "./components/layout";
+
 function App() {
-  return <div>Hello world</div>;
+  return <Layout HeaderComponent={null} MainComponent={null} />;
 }
 
 export default App;
---
+++ src/components/layout.jsx
@@ -0,0 +1,10 @@
+export function Layout({ HeaderComponent, MainComponent }) {
+  return (
+    <div className="px-24 h-screen flex">
+      <header className="border-2 border-red-500 grow">
+        {HeaderComponent}
+      </header>
+      <main className="border-2 border-yellow-500 grow">{MainComponent}</main>
+    </div>
+  );
+}
