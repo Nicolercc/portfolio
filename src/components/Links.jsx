a31cacc639b63039a4f0e4171ce855460e8221cd
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-09-24 16:04:05 -0400

    added labels

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,19 +5,19 @@ function Links() {
     <div>
       <ul className="text-gray-600  font-semibold">
         <li className="py-4">
-          <a href="#About" rel="">
+          <a href="#About" rel="about">
             {" "}
             ABOUT
           </a>
         </li>
         <li className="py-4">
-          <a href="#Experience" rel="">
+          <a href="#Experience" rel="experience">
             {" "}
             EXPERIENCE
           </a>
         </li>
         <li className="py-4">
-          <a href="#Projects" rel="">
+          <a href="#Projects" rel="projects">
             PROJECTS
           </a>
         </li>
