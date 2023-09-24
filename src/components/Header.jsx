de55fc60cdfba90fc5fc3e22258ad8df36ec3a1e
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-09-24 14:04:01 -0400

    added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="py-24">
+      <h1 className="text-7xl font-bold py-4">Nicole Rodriguez</h1>
+      <p className="text-4xl font-semibold py-3">Jr. Full-stack Engineer</p>
+      <p className="text-2xl py-3">
+        I build accessible, inclusive products and digital experiences for the
+        web.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,9 @@
+import Header from "./Header";
+
 export function Layout({ HeaderComponent, MainComponent }) {
   return (
     <div className="px-24 h-screen flex">
-      <header className="border-2 border-red-500 grow">
-        {HeaderComponent}
-      </header>
+      <header className="border-2 border-red-500 grow">{<Header />}</header>
       <main className="border-2 border-yellow-500 grow">{MainComponent}</main>
     </div>
   );
