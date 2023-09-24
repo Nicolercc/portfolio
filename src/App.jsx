0b52bff2b53801c52c6629736bea2e83792be8d3
josemlrod <mrjosemlrod@gmail.com>
2023-09-24 14:07:24 -0400

    update usage of Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Header from "./components/Header";
 import { Layout } from "./components/layout";
 
 function App() {
-  return <Layout HeaderComponent={null} MainComponent={null} />;
+  return <Layout HeaderComponent={<Header />} MainComponent={null} />;
 }
 
 export default App;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,7 +3,9 @@ import Header from "./Header";
 export function Layout({ HeaderComponent, MainComponent }) {
   return (
     <div className="px-24 h-screen flex">
-      <header className="border-2 border-red-500 grow">{<Header />}</header>
+      <header className="border-2 border-red-500 grow">
+        {HeaderComponent}
+      </header>
       <main className="border-2 border-yellow-500 grow">{MainComponent}</main>
     </div>
   );
